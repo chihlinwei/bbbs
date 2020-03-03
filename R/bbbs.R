8c63a03b432b309e0113453923c6dd94554d351b
Chih-Lin Wei <chihlinwei@gmail.com>
2020-03-03 17:52:43 +0800

    Initial commit

---
+++ R/bbbs.R
@@ -0,0 +1,7 @@
+#' Package for analyzing benthic biomass and body size
+#'
+#' tbbs package contains data and code to analyze benthic biomass and body size off SW Taiwan.
+#'
+#' @author Chih-Lin Wei <chihlinwei@@gmail.com>, Jian-Xiang Liao <jianxiangliao@@gmail.com >
+#' @name bbbs
+NULL
